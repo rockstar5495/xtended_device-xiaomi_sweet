cff824c0845f1925bf59a4d31b795df6b81a790b
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-10-09 00:37:14 +0530

    Revert "sweet: props: move RIL to system"
    This reverts commit c356e4370b0e1554e20e446233b010d593459a0c.
    
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -25,15 +25,3 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.debuggable=1 \
     persist.service.adb.enable=1 \
     persist.sys.usb.config=adb,mtp
-
-# RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-  persist.dbg.volte_avail_ovr=1 \
-  persist.dbg.vt_avail_ovr=1 \
-  persist.dbg.wfc_avail_ovr=1 \
-  persist.vendor.radio.enable_temp_dds=true \
-  persist.vendor.radio.report_codec=1 \
-  persist.vendor.ims.disableADBLogs=1 \
-  persist.vendor.ims.disableDebugLogs=1 \
-  persist.vendor.ims.disableIMSLogs=1 \
-  persist.vendor.ims.disableQXDMLogs=1
